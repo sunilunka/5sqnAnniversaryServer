fbc060f4de2f67b4dea0d51ca4874fb633995540
Sunil Unka <sunil.unka@gmail.com>
2016-07-10 00:56:32 +0100

    Working on CORS functionality for server.

--- server/app/configure/middleware.js
+++ server/app/configure/middleware.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var bodyParser = require('body-parser');
+var firebaseInit = require('./authentication/firebase_config');
 
 module.exports = function(app){
+  firebaseInit();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
 }
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,13 +4,22 @@ var express = require('express');
 
 var router = express.Router();
 
+var firebase = require('firebase');
+
 
 router.get('/', function(req, res, next){
-  console.log("REQUEST: ", req.headers)
-  res.set({
-    'Access-Control-Allow-Origin': 'http://localhost:1337'
+  console.log("REQUEST: ", req.headers);
+  var db = firebase.database();
+  var attendeesRef = db.ref('/attendees');
+  var attendees = "";
+  attendeesRef.on("value", function(snapshot){
+    attendees = snapshot.val();
+    attendees = JSON.stringify(attendees)
+    res.set({
+      'Access-Control-Allow-Origin': '*'
+    })
+    res.status(200).json(attendees);
   })
-  res.status(200).json('SERVER HAS RESPONDED');
 })
 
 
