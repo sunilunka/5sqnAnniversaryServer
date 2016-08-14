a9139546870e1727f1a822bd576c18704939a7a6
Sunil Unka <sunil.unka@gmail.com>
2016-08-14 21:07:38 +0100

    Completed route for sending an email to a new user.

--- server/app/routes/email.js
+++ server/app/routes/email.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var express = require('express');
 var router = express.Router();
-
+var fireMethods = require(path.join(__dirname, '../../db/fire-db'));
 var mailer = require(path.join(__dirname, '../email'));
 
 
@@ -13,7 +13,16 @@ router.post('/group', function(req, res, next){
 })
 
 router.post('/register-success', function(req, res, next){
-
+  fireMethods.getOneAttendeeRef(req.body.userId)
+  .then(function(user){
+    return mailer.generateNewAttendeeEmail(user);
+  })
+  .then(function(status){
+    if((status > 199) && (status < 300)){
+      res.sendStatus(200);
+    }
+  })
+  .catch(next);
 })
 
 
