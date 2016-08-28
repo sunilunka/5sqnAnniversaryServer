a8b9a3aa2272bd11aa2be97b5bc7a57eeb766d9e
Sunil Unka <sunil.unka@gmail.com>
2016-08-28 17:32:52 +0100

    Have basic PDF generation working, now looking at downloading.

--- server/app/routes/file-generation.js
+++ server/app/routes/file-generation.js
@@ -7,6 +7,8 @@ var router = express.Router();
 var fireMethods = require(path.join(__dirname, '../../db/fire-db'));
 var fileGenMethods = require(path.join(__dirname, '../file-generation'));
 var fs = require('fs-extra');
+var pdfGen = require('html-pdf');
+var phantom = require('phantom');
 
 router.get('/guest-list/:eventId', function(req, res, next){
   console.log("REQ PARAMS: ", req.params);
@@ -15,9 +17,35 @@ router.get('/guest-list/:eventId', function(req, res, next){
     res.render('guestlist.njk', {
       guestlist: guestListArray
     }, function(err, html){
-      var fileLocation = path.join(__dirname, '../../downloads/guestlist.pdf');
+      var fileLocation = path.join(__dirname, '../../downloads/guestlist.html');
       fs.outputFile(fileLocation, html, function(err){
-        res.download(fileLocation)
+        if(err) return next(err);
+        var instance;
+        var pageToPrint;
+        phantom.create()
+        .then(function(phInstance){
+          instance = phInstance;
+          return phInstance.createPage()
+        })
+        .then(function(page){
+          pageToPrint = page;
+          return page.open(fileLocation);
+        })
+        .then(function(status){
+          console.log("STATUS: ", status)
+          var savedDate = Date.now();
+          var fileName = savedDate + '-guestlist.pdf'
+          var renderPath = path.join(__dirname, '../../../downloads/' + );
+          pageToPrint.render(renderPath);
+          res.status(200).json({ assetPath: '/downloads/' });
+          return renderPath;
+        })
+        .then(function(){
+
+        })
+        .catch(function(err){
+          phInstance.exit();
+        })
       })
     });
   })
